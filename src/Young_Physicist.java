39a5f1a709b3da30aacfa0636c3c38da8a970d3d
Monish-Y <monishy24@gmail.com>
2024-08-11 17:19:04 +0530

    Young Physicist file is updated

--- src/Young_Physicist.java
+++ src/Young_Physicist.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Young_Physicist {
     public static void main(String[] args)
     {
-
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int x=0,y=0,z=0;
+        for(int i=1;i<=n;i++)
+        {
+            x = x+in.nextInt();
+            y = y+in.nextInt();
+            z = z+in.nextInt();
+        }
+        if(x==0&&y==0&&z==0)
+        {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
     }
 }
